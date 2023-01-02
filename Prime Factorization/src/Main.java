c2bd0a6b2b86407559971a971414a92c24e03c28
Van Vincent Vasquez <113902421+VanVasquez@users.noreply.github.com>
2023-01-02 22:55:42 +0800

    Update Main.java

--- Prime Factorization/src/Main.java
+++ Prime Factorization/src/Main.java
@@ -1,3 +1,13 @@
+/*
+    10. Prime factorization
+        The prime factors of a number are all of the integers below that number that are divisible into the number 
+        as well as 1. For example, the prime factors of 12 are 1,2,3,4,6, and 12.
+
+        Create a prime factorization calculator that returns the prime factors of any number between 2 and 100. 
+        If you're looking for a more advanced version of this challenge, use exercise 9 to create a prime factorization 
+        calculator for any number. Hint: think about using square roots to cut your work in half.
+*/
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
