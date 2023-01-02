c2fb76d0e2549b6fd9ef1159dd6c5407f6f600be
Van Vincent Vasquez <113902421+VanVasquez@users.noreply.github.com>
2023-01-02 20:17:52 +0800

    Update Main.java

--- Word Reversal/src/Main.java
+++ Word Reversal/src/Main.java
@@ -1,3 +1,12 @@
+/*
+    1. Word reversal
+        For this challenge, the input is a string of words, and the output should be the words in reverse 
+        but with the letters in the original order. For example, the string “Dog bites man” should output as “man bites Dog.”
+
+        After you’ve solved this challenge, try adding sentence capitalization and punctuation to your code. 
+        So, the string “Codecademy is the best!” should output as “Best the is Codecademy!”
+*/
+
 import java.util.Scanner;
 
 public class Main {
