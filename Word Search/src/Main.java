ed4a353a3e2191ebbb2842fd472de4e4f31d68bd
Van Vincent Vasquez <113902421+VanVasquez@users.noreply.github.com>
2023-01-02 20:33:36 +0800

    Update Main.java

--- Word Search/src/Main.java
+++ Word Search/src/Main.java
@@ -1,3 +1,12 @@
+/*
+    3. Word search
+        For a given input string, return a Boolean TRUE if the string starts with a given input word. 
+        So, for an input string of “hello world” and input word “hello,” the program should return TRUE.
+
+        For a more advanced word searcher, create a program that returns the number of times a word appears in an input string.
+        For example, given an input word “new” and an input string “I'm the new newt,” the program should return a value of 2.
+*/
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
